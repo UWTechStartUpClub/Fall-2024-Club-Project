43be3bb9f42dce822ebd0030d81481bed2d4cb96
celestinryf <celestinryf@gmail.com>
2024-10-07 13:11:58 -0700

    Added some more defined javadoc to app.js file. created form submissions for contact tab. created API key alongside a gitignore and ejs file for the API key.

--- app.js
+++ app.js
@@ -18,8 +18,29 @@ app.get('/contact', (req, res) => {
   res.render('contact', { title: 'Contact Us' });
 });
 
-// Set the port and start the server
+/** Set the port and start the server */
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+/** Body parser */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/** helmet */
+const helmet = require('helmet');
+app.use(helmet());
+
+
+// Form submissions
+
+/** Contact form submission */
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  // Process form data (e.g., save to database, send email, etc.)
+  res.send(`Thank you, ${name}, we will get back to you soon!`);
+});
+
+
+// Connecting Databases
