2a8c0c2c7ca69cd73de8afe8153bdb02d4ded547
celestinryf <celestinryf@gmail.com>
2024-10-07 15:22:52 -0700

    Fixed issue of website not running. Changed the order of the code in the app.js file.

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express');
+const { getStockData } = require('./AlphaVantageService');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-// Define routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About Us' });
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact Us' });
-});
-
-/** Set the port and start the server */
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 /** Body parser */
 const bodyParser = require('body-parser');
@@ -33,6 +16,42 @@ const helmet = require('helmet');
 app.use(helmet());
 
 
+// Define routes
+
+/** Stock symbol */
+app.get('/stock/:symbol', async (req, res) => {
+  const symbol = req.params.symbol; // Get the stock symbol from the request URL
+  try {
+    const stockData = await getStockData(symbol);
+    res.json(stockData);  // Send the data as JSON
+  } catch (error) {
+    res.status(500).send('Error fetching stock data');
+  }
+});
+
+/** about route */
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About Us' });
+});
+
+/** contact route */
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+/** Set the port and start the server */
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 // Form submissions
 
 /** Contact form submission */
@@ -41,6 +60,3 @@ app.post('/contact', (req, res) => {
   // Process form data (e.g., save to database, send email, etc.)
   res.send(`Thank you, ${name}, we will get back to you soon!`);
 });
-
-
-// Connecting Databases
